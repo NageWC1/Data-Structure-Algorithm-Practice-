6913ed3514c99c15646f575c78c91b44eeabfe39
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-03-24 17:47:35 +0530

    Started the practice for the linked list

---
+++ linked_list_p.py
@@ -0,0 +1,12 @@
+# implement the linked list using python 
+class Node:
+    def __init__ (slef, data=None, next=None):
+        self.data = data 
+        self.next = next 
+
+class LinkedList:
+    def __init__ (slef):
+        self.head = None
+
+if __name__ == '__main__':
+    pass 
