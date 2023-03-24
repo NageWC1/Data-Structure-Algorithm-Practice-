84c7f2063afee5c005d5ba1b214de2304297e7fc
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-03-24 18:47:27 +0530

    In the linked list implementation the insert at start, end, then priniting the data in the linked list and get the length of the list are done

--- linked_list_p.py
+++ linked_list_p.py
@@ -9,6 +9,7 @@ class Node:
 # we have head variable, which points to the heade the of the linked list 
 class LinkedList:
     def __init__ (self):
+        # slef head is a object that going to the beginning of the node of link list 
         self.head = None
 
     # this is responsible for inserting value to beggining of the list  
@@ -16,22 +17,54 @@ class LinkedList:
         node = Node(data, self.head)
         self.head = node 
     
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None # wipe out all the value from the list 
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr  = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        
+        return count
+
     def print(self):
         if self.head is None:
             print("the link lis is empty")
             return
+        
         itr = self.head
+
         llstr = ''
         while itr:
             llstr += str(itr.data) + '-->'
             itr = itr.next
 
         print(llstr)
+
+
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insert_at_beggining(5)
-    ll.insert_at_beggining(15)
-    ll.insert_at_beggining(10)
-    ll.insert_at_beggining(12)
+    # ll.insert_at_beggining(5)
+    # ll.insert_at_beggining(15)
+    # ll.insert_at_beggining(10)
+    # ll.insert_at_end(154)
+    # ll.insert_at_beggining(12)
+    ll.insert_values(["hi","bye"])
+    print("my linked list length is",ll.get_length())
     ll.print()
 
