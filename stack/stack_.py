5220ac3064d00cd0fb494e5e3c66deb342fc0e8e
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-03-26 18:23:09 +0530

    the stack implementation with deque also done, now just have to do the practices

--- stack/stack_.py
+++ stack/stack_.py
@@ -16,6 +16,12 @@ class stack_:
     def print(self):
         return self.links_
     
+    def is_empty(self):
+        return len(self.links_) == 0
+    
+    def size(self):
+        return len(self.links_)
+    
 if __name__ == '__main__':
     stack1 = stack_()
     print(stack1.pop())
@@ -23,5 +29,20 @@ if __name__ == '__main__':
     print(stack1.pop())
     print(stack1.push(20))
     print(stack1.print())
+    print(stack1.is_empty())
+    print(stack1.size())
 
+
+
+    from collections import deque
+    stack = deque()
+
+    # dir(stack) - will show the functions/ methods that we can use to manipulate the stack
+    stack.append('link5')
+    stack.append('link6')
+    stack.append('link7')
+    stack.append('link8')
+    print(stack)
+    print(stack.pop())
+    print(stack)
 
