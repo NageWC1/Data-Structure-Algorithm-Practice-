8cf28b4c5ef832babd0abb91d96aaa2eff9df769
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-03-27 05:19:37 +0530

    have started the implementation for the tree datastrcture, the basic set up was doe now move on tot the practice

---
+++ Tree/tree.py
@@ -0,0 +1,11 @@
+# here all the tree data strcuture will be implemented 
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+
+
+if __name__ == '__main__':
+    print('this will be the main function')
