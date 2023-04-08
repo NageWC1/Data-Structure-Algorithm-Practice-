d3c75cf6bf2cdb08da7da4ca18c2d9dd945f0e7b
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-04-08 14:58:25 +0530

    added the implementation of shell sort to the project, all working perfectly

--- Sorting Algos/shell_sort/shell_sort.py
+++ Sorting Algos/shell_sort/shell_sort.py
@@ -1,20 +1,32 @@
 def shell_sort(arr):
+    
     size = len(arr)
     gap = size // 2
-
+    
     while gap > 0:
         for i in range(gap, size):
             anchor = arr[i]
             j = i
-
             while j >= gap and arr[j-gap] > anchor:
-                arr[j] == arr[j-gap]
-                j -= gap   
+                arr[j] = arr[j-gap]
+                j -= gap
             arr[j] = anchor
         gap = gap // 2
+
 
 
 if __name__ == "__main__":
     elements = [21,38,29,17,4,25,11,32,9]
     shell_sort(elements)
     print(elements)
+
+    test = [
+        [89,78,34,56,43,23,24,4,5,3,24,2],
+        [67,23,45,12,78,43],
+        [],
+        [23,12,34,2,5,3,12,32,3]
+    ]
+
+    for elements in test:
+        shell_sort(elements)
+        print(f'The sorted array: {elements}')
